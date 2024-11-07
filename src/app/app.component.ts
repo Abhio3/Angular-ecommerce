e24bf1642e59cc62ebe8f331aca27ead048adfea
Abhio3 <aabhisheknair2003@gmail.com>
2024-11-07 19:16:48 +0530

    Copleted with nav and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PolicyComponent } from './policy/policy.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, PolicyComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
   title = 'ecommerce';
-}
+  url:string = "images/img-4.jpg"
+  changeImg(event:any){
+  this.url =event.target.src
+   }
+   
+   url2:string = "images/img-5.jpg"
+    changeImg2(event:any){
+   this.url2 =event.target.src
+  }
+    
+  url3:string = "images/img-9.jpg"
+  changeImg3(event:any){
+   this.url3 =event.target.src
+    }
+
+    url4:string = "images/img-13.jpg"
+    changeImg4(event:any){
+     this.url4 =event.target.src
+      }
+ 
+  }
